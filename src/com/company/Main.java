caa246bad30f112556b5bbe309b1e0f469a3011c
Danny <mezenov.201521@gmail.com>
2019-10-16 14:06:20 +0300

    deleteAll

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,14 +6,6 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        //System.out.print("Vvedite radius ocrugnostui: ");
-        Scanner in = new Scanner(System.in);
-        in.useLocale(Locale.US);
-
-
-        double a = in.nextDouble();
-        double b = in.nextDouble();
-        //System.out.println("Result of calculating: "+(Math.PI*r*r + Math.PI*r1*r1));
-        System.out.println(a+b);
+        
     }
 }
