d42f66d1e057528b6daf25e2e880746a0dd75d9d
Danny <mezenov.201521@gmail.com>
2019-10-16 14:36:13 +0300

    Min and Max found

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,7 +6,21 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        
+        double max = -1;
+        double min = 1.1;
+
+        for (int i = 0; i< 10; i++){
+            double a = Math.random();
+            System.out.println(a);
+            if (a>max){
+                max = a;
+            }
+            if (a<min){
+                min = a;
+            }
+        }
+
+        System.out.println("Max: "+ max + " Min: "+ min);
 
     }
 }
