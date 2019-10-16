430b1b2cbd7e45348e392499a62a9d8fa19dd0dc
Danny <mezenov.201521@gmail.com>
2019-10-16 14:04:37 +0300

    some testes

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,15 +1,19 @@
 package com.company;
 
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
+        //System.out.print("Vvedite radius ocrugnostui: ");
         Scanner in = new Scanner(System.in);
-        int a = in.nextInt();
-        int b = in.nextInt();
+        in.useLocale(Locale.US);
+
+
+        double a = in.nextDouble();
+        double b = in.nextDouble();
+        //System.out.println("Result of calculating: "+(Math.PI*r*r + Math.PI*r1*r1));
         System.out.println(a+b);
-	System.out.println("Hello, World!");
-	System.out.println("My name is...");
     }
 }
