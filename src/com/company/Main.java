52ab1d9be0623ac5cf3beaf87b37b247c0b0eacb
Danny <mezenov.201521@gmail.com>
2019-10-16 14:27:21 +0300

    String Tests

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,6 +6,27 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        /*long x = 1000*10000;
+        System.out.println(x);
+
+        System.out.println(Math.abs(-7)%3);*/
+
+        int a = 8;
+        int b = 3;
+
+        System.out.println(a/b);
+
+
+
+        String string = "qwerty";
+        String string2 = new String(string);
         
+        char ch = '1';
+
+        int c = ch;
+        System.out.println("This is char: " + ch);
+        System.out.println("This is char UNICODE position: " + c);
+        System.out.println((char)(c + ch));
+
     }
 }
